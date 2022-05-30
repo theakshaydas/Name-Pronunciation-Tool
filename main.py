476d6f770f40aa22e3f315ab385fe7c587433121
deepcadence22 <105427068+deepcadence22@users.noreply.github.com>
2022-05-30 15:28:10 +0530

    added few more dependent requirements

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ import flask_app
 
 if __name__ == "__main__":
     # run_tests()
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, port=8080)
 
